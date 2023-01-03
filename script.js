9977a4ff72c31e8486e15b5a91cc2028cd0d2f32
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-01-03 11:40:34 -0500

    java script

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const show = document.querySelector(".hide");
 const input = document.getElementById("input").value;
 const match = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/
 const email = document.getElementById("email");
-let element = document.createElement("p");
-
+let element = document.createElement("div");
+const write  = document.getElementById("write");
 buttion.addEventListener("click", function() {
     if(match.test(input) == false) {
-        show.classList.toggle('icon');
-        
+        show.classList.add('icon');   
+        element.id = "new-element"
+        element.style.color = "red";
+        element.innerText = "Please provide valid Emali..."
+        email.appendChild(element);
+    }else {
+        window.location.reload()
     }
 })
